46f05c8528f2876be6b3002a388674f8cf3a901e
Ana Belen Alfonso <anabelen.alfonso7@gmail.com>
2021-02-28 15:38:53 -0300

    cambios menores

--- GYM/apps/registro/views.py
+++ GYM/apps/registro/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import registrar_cliente
 from .models import usuario
-from django.contrib.auth import login
-from django.contrib.auth import logout
 from django.views.generic import CreateView
-from .models import registro
 
-
-
-class login(CreateView):
-	model = registro
-	form_class = registrar_cliente
-	template_name = 'login.html'
-
-def logout_view(request):
-    logout(request)
+def registro(request):
+	return registrar_cliente
 
 class registro_usuario(CreateView):
-	model =usuario
+	model = usuario
 	form_class = registrar_cliente
 	template_name = 'registro.html'
