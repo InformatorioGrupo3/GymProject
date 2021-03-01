cf7a788522a748535ab774b33fe1530848187f00
Mluchy <luchy.isi@gmail.com>
2021-02-28 23:27:38 -0300

    login template para probar

--- GYM/apps/login/views.py
+++ GYM/apps/login/views.py
@@ -2,12 +2,14 @@
 from django.shortcuts import render
 from apps.registro.models import usuario
 from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout
+
 
 # Create your views here.
 def login(request):
     user = usuario.objects.get(nombre=request.POST['nombre'])
+    user.contraseña == request.POST['contraseña']
     if user.contraseña == request.POST['contraseña']:
         request.session['usuario_id'] = user.id
         return HttpResponse("¡ Bienvenido !", 'index.html')
