f254485ce6be20d1d6c9d735b0eb91f8d1366f34
Mluchy <luchy.isi@gmail.com>
2021-03-02 23:58:35 -0300

    trabajando en template turno

--- GYM/apps/turno/views.py
+++ GYM/apps/turno/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from .forms import registrar_turno
-from .models import turno
+#from .forms import registrar_turno
+#
+# class registro(CreateView):
+# 	model = turno
+# 	form_class = registrar_turno
+# 	template_name = 'turno.html'
 
-class registro(CreateView):
-	model = turno
-	form_class = registrar_turno
-	template_name = 'turno.html'
-	
+
+def turno(request):
+	return render(request,'turno.html')
