ca9fc9a401e7caa36b785805a6fea8e4f41c8256
EGusb <emggw@hotmail.com>
2021-03-05 15:04:05 -0300

    modifico modelos actividades y turno

--- GYM/apps/turno/models.py
+++ GYM/apps/turno/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from apps.registro.models import usuario
 
-class actividades(models.Model):
+class actividades(models.Model):   
+    nombre = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    cupo_max = models.PositiveSmallIntegerField(
+        verbose_name='Cupo máximo de personas',
+        blank=False,
+        null=False
+        )
+
+#Lo que está comentado no sirve, se agregan actividades desde el admin
+'''
     Zumba = 'Zumba'
     Funcional = 'Funcional'
     Aerobic = 'Aerobic'
@@ -20,13 +29,20 @@ class actividades(models.Model):
         choices = actividades_gym,
         default = None,
     )
+'''
 
+class turno(models.Model):
+    usuario = models.ManyToManyField(usuario, verbose_name='usuarios en este turno', name='usuario')
+    actividad = models.ForeignKey(actividades, on_delete=models.CASCADE, name='actividad')
+    #cupo_max = actividades.cupo_max
+    cupo_actual = models.PositiveSmallIntegerField(default=0)
+    horario = models.DateTimeField(auto_now=True)
+    disponible = models.BooleanField(default=True)
 
-class turno(models.Model, usuario, actividades):
-    usuario = models.ManyToManyField(usuario, on_delete=models.CASCADE)
-    actividad = models.ForeignKey(actividades, on_delete=models.CASCADE)
-    cupo = models.CharField(max_length=50)
-    horario = models.DateField(auto_now_add=True)
+    class Meta:
+        db_table = 'turnos'
+        ordering = ['actividad', 'horario']
 
     def __str__(self):
         return super.usuario, 'Actividad: {}\n Horario:{}'.format(self.actividad, self.horario)
+
