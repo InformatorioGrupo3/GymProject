8042fe2c4dfef3543cc0049e4726e0065ad42974
EGusb <emggw@hotmail.com>
2021-03-06 08:58:46 -0300

    gitignore fix

--- GYM/apps/login/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from apps.login.models import *
-
-# Register your models here.
-#admin.site.register()
--- GYM/apps/registro/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.registro.models import *
-
-# Register your models here.
-
-class AdminUsuario(admin.ModelAdmin):
-    list_display = ('apellido', 'nombre', 'habilitado', 'dni', 'fecha_nacimiento')
-
-admin.site.register(usuario, AdminUsuario)
