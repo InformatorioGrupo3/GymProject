86563d81eeea89ab9036fb352366b88435b0560d
EGusb <emggw@hotmail.com>
2021-03-06 18:18:14 -0300

    formato pagina admin

--- GYM/apps/registro/admin.py
+++ GYM/apps/registro/admin.py
@@ -4,6 +4,7 @@ from apps.registro.models import *
 # Register your models here.
 
 class AdminUsuario(admin.ModelAdmin):
-    list_display = ('apellido', 'nombre', 'habilitado', 'dni', 'fecha_nacimiento')
+    list_display = ('apellido', 'nombre', 'habilitado', 'dni', 'fecha_nacimiento',)
+    list_filter = ('habilitado',)
 
 admin.site.register(usuario, AdminUsuario)
