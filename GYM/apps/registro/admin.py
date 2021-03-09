6e0026e279c066701d0e2675d6be1f696453c43f
EGusb <emggw@hotmail.com>
2021-03-08 21:45:35 -0300

    modifico admin y models

--- GYM/apps/registro/admin.py
+++ GYM/apps/registro/admin.py
@@ -4,8 +4,8 @@ from apps.registro.models import *
 # Register your models here.
 
 class AdminUsuario(admin.ModelAdmin):
-    #list_display = ('last_name', 'first_name', 'habilitado', 'dni', 'fecha_nacimiento', 'password',)
-    list_display = ('apellido', 'nombre', 'habilitado', 'dni', 'fecha_nacimiento', 'password',)
+    list_display = ('last_name', 'first_name', 'habilitado', 'dni', 'fecha_nacimiento', 'password',)
+    #list_display = ('apellido', 'nombre', 'habilitado', 'dni', 'fecha_nacimiento', 'password',)
     list_filter = ('habilitado',)
 
 admin.site.register(usuario, AdminUsuario)
--- GYM/apps/registro/models.py
+++ GYM/apps/registro/models.py
@@ -3,10 +3,10 @@ from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django import forms
 
-
+'''
 class usuario(models.Model):
-    nombre = models.CharField(max_length=50, blank=False)
-    apellido = models.CharField(max_length=50, blank=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
     fecha_nacimiento = models.DateField(blank=True, null=True)
     dni = models.PositiveIntegerField(unique=True, blank=False, help_text='DNI sin puntos')
     password = models.CharField(null=False, max_length=30)
@@ -23,8 +23,8 @@ class usuario(models.Model):
 
     def __str__(self):
         return f'{self.apellido}, {self.nombre} - DNI: {self.dni}'
-
 '''
+
 class usuario(User):
     #first_name = models.CharField(max_length=50, blank=False)
     #last_name = models.CharField(max_length=50, blank=False)
@@ -44,4 +44,3 @@ class usuario(User):
 
     def __str__(self):
         return f'{self.apellido}, {self.nombre} - DNI: {self.dni}'
-'''
